3246c6bb76be57edce1db04fb63a9264af20bb41
zhaoyifeng <zhaoyf@weipaitang.com>
2020-04-22 17:10:34 +0800

    perf👌:调整代码结构

--- build/devServer.js
+++ build/devServer.js
@@ -5,8 +5,9 @@ const webpackHotMiddleware = require('webpack-hot-middleware')
 const config = require('./webpack.dev.js')
 
 const complier = webpack(config)
-
 const app = express()
+
+// rewite 入口文件,加入热更新代码
 
 app.use(
   webpackDevMiddleware(complier, {
@@ -17,6 +18,8 @@ app.use(
 
 app.use(webpackHotMiddleware(complier))
 
-app.listen(3000, () => {
-  console.log('服务已运行在3000端口,打开 http://localhost:300')
+app.listen(config.devServer.port, () => {
+  console.log(
+    `服务已运行在${config.devServer.port}端口,打开 http://localhost:${config.devServer.port}`
+  )
 })
--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -3,23 +3,29 @@ const webpack = require('webpack')
 const path = require('path')
 const base = require('./base')
 
-// 重构 entry
 // 加入热更新头
 // 'webpack-hot-middleware/client'
 const { entry } = base
-const hotEntry = {}
-Object.keys(entry).forEach(key => {
-  let item = entry[key]
-  if (item instanceof Array) {
-    item.unshift('webpack-hot-middleware/client?reload=true')
-  } else {
-    item = ['webpack-hot-middleware/client?reload=true', item]
-  }
-  hotEntry[key] = item
-})
+const hotJS = 'webpack-hot-middleware/client?reload=true'
+if (entry instanceof Array) {
+  entry.unshift(hotJS)
+} else {
+  Object.keys(entry).forEach(key => {
+    let item = entry[key]
+    if (item instanceof Array) {
+      item.unshift(hotJS)
+    } else {
+      item = [hotJS, item]
+    }
+    entry[key] = item
+  })
+}
+
+base.entry = entry
+
 // console.log('base', hotEntry)
 module.exports = merge(base, {
-  entry: hotEntry,
+  // entry: hotEntry,
   devServer: {
     hot: true,
     host: 'localhost',
